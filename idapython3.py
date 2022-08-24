2b32f3522d7104b4e38a6d168786e338c807d1e0
어그래왔니 <69739898+kal501@users.noreply.github.com>
2022-08-24 17:57:38 +0900

    Update idapython3.py
    Step Into

--- idapython3.py
+++ idapython3.py
@@ -6,3 +6,12 @@ evt_code = idc.wait_for_next_event(WFNE_SUSP, -1) # event code
 if evt_code > 0 and evt_code != idc.PROCESS_EXITED:
     evt_ea = idc.get_event_ea()
     print("Breaking Point Triggered at : ",hex(evt_ea),idc.generate_disasm_line(evt_ea,0))
+
+#### Step Into 
+# Next Instruction
+idc.step_into()
+evt_code = idc.wait_for_next_event(WFNE_SUSP, -1)
+if evt_code > 0 and evt_code != idc.PROCESS_EXITED:
+    evt_ea = idc.get_event_ea()
+    print("Stepped Into : ", hex(evt_ea),idc.generate_disasm_line(evt_ea,0))
+
